3f01e766c5008bfc2389c567e2d393a6be59d811
Roman <roman.charugin@acronis.com>
2026-08-08 17:11:33 +0300

    fixup! Feat: [#341] Convert to ESM with dual CJS/ESM exports

--- examples/upload.js
+++ examples/upload.js
@@ -1,8 +1,10 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 import { upload } from '../index.js';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const { API_TOKEN = '' } = process.env;
 const fileToUpload = fs.createReadStream(path.join(__dirname, 'upload.js'));
 
